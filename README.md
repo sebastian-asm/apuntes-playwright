46687c6527c17b6bca0c63881683cdf4c6cc948a
Sebastian Sanchez <sasanchezmiles@teco.com.ar>
2025-05-22 18:47:04 -0300

    validaciones

--- README.md
+++ README.md
@@ -19,7 +19,7 @@ Hay herramientas que permiten probar de principio a fin los flujos de usuarios d
 - `getByAltText`: buscar por el atributo _alt_ en una imagen.
 - `getByTitle`: buscar por el atributo _title_ en una etiqueta.
 - `getByTestId`: enfocado directamente para el testing, permite buscar por el atributo _data-testid_ por ejemplo: `<button data-testid="botonDemoId">Botón</button>`
-- `locator (css y xpath)`: selector no recomendado.
+- `locator (css y xpath)`: es recomendable solo utilizar cuando PW no tiene la herramienta necesaria para obtener un elemento.
 
 ## Filtros
 
@@ -45,3 +45,17 @@ page.getByRole('listitem).filter({ has: page.getByRole('heading', { name: 'Títu
 - `check`: permite marca un checkbox o seleccionar un radio button, el cual tiene su contraparte `uncheck` (pero este no funciona con los radio buttons).
 - `selectOption`: seleccionar un elemento de una lista Dropdown.
 - `setInputFiles`: testear la subida de un archivo, por ejemplo: `setInputFiles('miArchiv.pdf)` y en caso de simular varios archivos: `setInputFiles(['archivo1.pdf', 'archivo2.pdf'])`.
+
+## Assertions (verificaciones)
+
+Es cuando se realiza una verificación final del elemento seleccionado, y si este retorna el resultado esperado. Por cada prueba solo se debería probar una sola cosa, esto permitirá saber en caso que falle el test, en donde se encuentra rápidamente el problema. Existen Assetions con [auto-retrying](https://playwright.dev/docs/test-assertions#auto-retrying-assertions) y [non-retrying](https://playwright.dev/docs/test-assertions#non-retrying-assertions), esto quiere decir que se reintentará hasta que se complete o alcance el tiempo esperado. Es recomendable utilizar principalmente las que sí permite reintentar.
+
+Existen las **Soft Assertions** que son aserciones que fallan pero que no impiden el flujo total del test, por ejemplo: `expect.soft()`.
+
+- `expect`: asegurarse que el elemento seleccionado tenga el estado esperado. Se le puede enviar como 2 argumento un mensaje personalizado en caso de error, por ejemplo: `expect(element, 'Error al marcar la opción')`.
+  - `containsText`: matchea esa parte de texto
+  - `haveText`: matchea todo el texto
+  - `toBeVisible`: validando que un elemento sea visible
+  - `toBeEditable`: que un campo sea editable
+  - `toHaveValue`: obtener el valor de un input, existe también para obtener varios valores con `toHaveValues`
+  - `toHaveText`: obtener el texto de un elemento
--- tests/interaction.spec.ts
+++ tests/interaction.spec.ts
@@ -17,24 +17,44 @@ test.describe('Acciones en el Automation Sandbox', () => {
       await page
         .getByRole('button', { name: 'Hacé click para generar un ID dinámico y mostrar el elemento oculto' })
         .click()
+      await expect(page.getByText('OMG, aparezco después de 3')).toBeVisible()
     })
   })
 
-  test('Llenar un campo de textp', async ({ page }) => {
+  test('Llenar un campo de texto', async ({ page }) => {
+    const message = 'Mensaje de prueba'
     await test.step('Navegar al sitio web', async () => {
       await page.goto('https://thefreerangetester.github.io/sandbox-automation-testing/')
     })
     await test.step('Ingresar texto en el campo', async () => {
-      await page.getByPlaceholder('Ingresá texto').fill('Mensaje de prueba')
+      await expect(page.getByPlaceholder('Ingresá texto'), 'El campo de texto no es editable').toBeEditable()
+      await page.getByPlaceholder('Ingresá texto').fill(message)
+      await expect(page.getByPlaceholder('Ingresá texto')).toHaveValue(message)
     })
   })
 
-  test('Seleccionar checkbox', async ({ page }) => {
+  test('Seleccionar y deseleccionar checkbox', async ({ page }) => {
     await test.step('Navegar al sitio web', async () => {
       await page.goto('https://thefreerangetester.github.io/sandbox-automation-testing/')
     })
     await test.step('Seleccionar el checkbox', async () => {
-      await page.getByRole('checkbox', { name: 'Pasta 🍝' }).check()
+      const checkbox = page.getByRole('checkbox', { name: 'Pasta 🍝' })
+      await checkbox.check()
+      await expect(checkbox).toBeChecked()
+    })
+    await test.step('Deseleccionar el checkbox', async () => {
+      const checkbox = page.getByRole('checkbox', { name: 'Pasta 🍝' })
+      await checkbox.uncheck()
+      await expect(checkbox).not.toBeChecked()
+      // mensaje de error personalizado en el expect
+      // await expect(checkbox, 'El checkbox no estaba desmarcado').toBeChecked()
+    })
+    await test.step('Validando que los elementos del checkbox son correctos', async () => {
+      await expect.soft(page.getByText('Pizza123 🍕'), 'No se encontró el texto Pizza 🍕').toBeVisible()
+      await expect.soft(page.getByText('Hamburguesa 🍔')).toBeVisible()
+      await expect.soft(page.getByText('Pasta 🍝')).toBeVisible()
+      await expect.soft(page.getByText('Helado456 🍧'), 'No se encontró el texto Helado 🍧').toBeVisible()
+      await expect.soft(page.getByText('Torta 🍰')).toBeVisible()
     })
   })
 
@@ -56,6 +76,22 @@ test.describe('Acciones en el Automation Sandbox', () => {
     })
   })
 
+  test('Los items del Dropdown son los esperados', async ({ page }) => {
+    await test.step('Navegar al sitio web', async () => {
+      await page.goto('https://thefreerangetester.github.io/sandbox-automation-testing/')
+    })
+    await test.step('Validando la lista de items', async () => {
+      const options = ['Fútbol', 'Tennis', 'Basketball']
+      for (const option of options) {
+        const element = await page.$(`select#formBasicSelect option:is(:text("${option}"))`)
+        if (element) console.log(`El elemento ${option} existe`)
+        else throw new Error(`El elemento ${option} no existe`)
+      }
+      // const locator = page.locator('formBasicSelect')
+      // await expect(locator).toHaveValues(['Fútbol', 'Tennis', 'Basketball'])
+    })
+  })
+
   test('Seleccionar un item del Dropdown "Día de la semana"', async ({ page }) => {
     await test.step('Navegar al sitio web', async () => {
       await page.goto('https://thefreerangetester.github.io/sandbox-automation-testing/')
@@ -63,6 +99,49 @@ test.describe('Acciones en el Automation Sandbox', () => {
     await test.step('Seleccionar un item', async () => {
       await page.getByRole('button', { name: 'Día de la semana' }).click()
       await page.getByRole('link', { name: 'Lunes' }).click()
+    })
+  })
+
+  test('Validando la columna "Nombre" en la tabla estática', async ({ page }) => {
+    await test.step('Navegar al sitio web', async () => {
+      await page.goto('https://thefreerangetester.github.io/sandbox-automation-testing/')
+    })
+    await test.step('Validar los elementos de la columna', async () => {
+      const columnValues = await page.$$eval(
+        'h2:has-text("Tabla estática") + table tbody tr td:nth-child(2)',
+        (elements) => elements.map((element) => element.textContent)
+      )
+      const expectNames = ['Messi', 'Ronaldo', 'Mbappe']
+      expect(columnValues).toEqual(expectNames)
+    })
+  })
+
+  test('Validando los valores de la tabla dinámica', async ({ page }) => {
+    await test.step('Navegar al sitio web', async () => {
+      await page.goto('https://thefreerangetester.github.io/sandbox-automation-testing/')
+    })
+    await test.step('Validando que los valores cambiaron después de un reload', async () => {
+      const tableValues = await page.$$eval('h2:has-text("Tabla dinámica") + table tbody tr td', (elements) =>
+        elements.map((element) => element.textContent)
+      )
+      await page.reload()
+      const tableValuesReloaded = await page.$$eval('h2:has-text("Tabla dinámica") + table tbody tr td', (elements) =>
+        elements.map((element) => element.textContent)
+      )
+      expect(tableValues).not.toEqual(tableValuesReloaded)
+    })
+  })
+
+  test('Validando dentro del popup', async ({ page }) => {
+    await test.step('Navegar al sitio web', async () => {
+      await page.goto('https://thefreerangetester.github.io/sandbox-automation-testing/')
+    })
+    await test.step('Hacer click en el botón para abrir el popup', async () => {
+      await page.getByRole('button', { name: 'Mostrar popup' }).click()
+    })
+    await test.step('Validando un elemento dentro del popup', async () => {
+      await expect(page.getByText('¿Viste? ¡Apareció un Pop-up!')).toHaveText('¿Viste? ¡Apareció un Pop-up!')
+      await page.getByRole('button', { name: 'Close' }).click()
     })
   })
 })
